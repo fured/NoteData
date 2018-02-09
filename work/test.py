5ea8684fb066611052e043b0e84833d0d0173683
fured <emailfuli@126.com>
2018-02-09 11:32:41 +0800

    test

--- work/test.py
+++ work/test.py
@@ -1,18 +1,14 @@
-import socket,json
+import threading
+global asdfgh
+asdfgh = []
 
+def aa():
+    asdfgh.append('asdsaf')
 
-s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-#ts = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(("0.0.0.0",6666))
-#ts.connect(("192.168.1.103",12345))
-print "listen part 6666...."
+def bb():
+    aa()
+    print asdfgh
 
-while True:
-    data,addr = s.recvfrom(1 << 12)
-    print("From:{} \n {} \n".format(addr,data.decode()))
-    response = {"aaaaaa":"aaaaaaaaa","aaaaaaa":"aaaaaaaa"}
-    #ts.send(json.dumps(response))
-    print "response"
-    s.sendto(json.dumps(response),addr)    
-    
-
+if __name__ == '__main__':
+    t1 = threading.Thread(target=bb)
+    t1.start()
