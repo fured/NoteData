9f5d685ba400d9df1e6623abdcf134b1db2eddd1
fured <emailfuli@126.com>
2018-02-09 11:33:42 +0800

    modified

--- work/wifi_scan.sh
+++ work/wifi_scan.sh
@@ -1,8 +1,2 @@
-if [ -f /tmp/ssids ]; then
-    rm /tmp/ssids
-fi
-
-iw wlan0 scan | sort | uniq -c | sort -n |  awk -F ':' '/SSID:/ {print $2;}' > /tmp/ssids
-
+iw wlan0 scan | sort | uniq -c | sort -n |  awk -F ':' '/SSID:/ {print $2;}' > /tmp/ssids  
 cat /tmp/ssids
-
